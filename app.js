918353e373fc4a895260df36dabf23fcbd43218e
Brendon Richards <brendonrichards93@gmail.com>
2018-09-04 13:26:08 +0000

    Add and require body-parser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 // Require routes
 const productRoutes = require('./api/routes/products');
@@ -12,5 +13,21 @@ app.use(morgan('dev'));
 // Routes which will handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+// Error handling
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
