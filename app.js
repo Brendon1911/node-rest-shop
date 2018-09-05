c80b00caf983f469dca0ec902393dd7accf8a928
Brendon Richards <brendonrichards93@gmail.com>
2018-09-05 04:42:20 +0000

    Create product model and test

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // Require routes
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb://brendon1911-node-rest-shop-6366944/node-rest-shop', { useNewUrlParser: true });
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
